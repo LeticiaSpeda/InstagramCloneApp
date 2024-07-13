ddb937944a3c5f993b0820804f16766792160f89
Leticia Speda <leticiaspeda@gmail.com>
2024-07-13 18:42:01 -0300

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Leticia Speda on 13/07/24.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
